d00bcbd036751f3d943c2d8000559de92e0f19df
constantin <constantin.verine@gmail.com>
2023-09-30 18:46:19 +0200

    THE GOAT

--- Assets/Scripts/PlayerScript/PlayerMovement.cs
+++ Assets/Scripts/PlayerScript/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody _rb;
     private Vector2 _movementVelocity;
     private PlayerStats _stats;
+    private Animator _animator;
 
 
     private void Start()
@@ -17,19 +18,27 @@ public class PlayerMovement : MonoBehaviour
         _inputs = GetComponent<InputWrapper>();
         _rb = GetComponent<Rigidbody>();
         _stats = GetComponent<PlayerStats>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         _movementVelocity = _inputs.move * _stats.movementSpeed;
+        _animator.SetFloat("Velocity", _movementVelocity.magnitude);
+
+        Vector3 playerDirection = Vector3.right * _movementVelocity.x + Vector3.forward * _movementVelocity.y;
+        if (playerDirection.sqrMagnitude > 0.0f)
+        {
+            transform.rotation = Quaternion.LookRotation(playerDirection, Vector3.up);
+        }
     }
 
     private void FixedUpdate()
     {
         // Apply force to the rigidbody
         _rb.velocity += new Vector3(_movementVelocity.x, 0, _movementVelocity.y);
-        
+
         // Limit the velocity
         if (_rb.velocity.magnitude > _stats.movementSpeed)
         {

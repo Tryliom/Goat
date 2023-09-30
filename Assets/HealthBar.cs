6197c2f5f9ad666f775e9a56265dcdd842ee98a4
olivier <olivierpachoud2@gmail.com>
2023-09-30 16:30:29 +0200

    add healthBar 2

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
     private PlayerStats _playerStats;
-    
-    
+
+    [SerializeField] private Image _image;
     
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,7 @@ public class HealthBar : MonoBehaviour
 
     private void UpdateHealthBar(PlayerStats playerStats)
     {
-        
+        _image.fillAmount = 
     }
     
     // Update is called once per frame

ed2c3417bad64eda49a093d87274afe0d7bc5c32
olivier <olivierpachoud2@gmail.com>
2023-09-30 21:14:36 +0200

    add frequency values for shooter

--- Assets/Scripts/Projectiles/ProjectileShooter.cs
+++ Assets/Scripts/Projectiles/ProjectileShooter.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class ProjectileShooter : MonoBehaviour
 {
@@ -12,23 +15,43 @@ public class ProjectileShooter : MonoBehaviour
         Count
     }
 
+    [Header("Projectiles Ref")]
     [SerializeField] private LinearProj _linearProj;
     [SerializeField] private LinearProj _burstProj;
     [SerializeField] private SeekerHeadProj _seekerHeadProj;
 
-    [SerializeField] private float _spawnFrequency = 2f;
+    [Header("Projectiles values")]
     [SerializeField] private float _burstProjFrequency = 0.4f;
-
     [SerializeField] private int _burstProjNbr = 3;
-
+    
+    [Header("Spawn Frequency")]
+    [SerializeField] private float _baseMinSpawnFrequency = 5f;
+    [SerializeField] private float _baseMaxSpawnFrequency = 7f;
+    private float _minSpawnFrequency;
+    private float _maxSpawnFrequency;
+    private float _currentFrequency;
+    [SerializeField] private float _spawnFrequencyDiminution;
+    
+    
     private float _elapsedTime;
+    private float _totalTime;
+
+    private void Awake()
+    {
+        _currentFrequency = Random.Range(_baseMinSpawnFrequency, _baseMaxSpawnFrequency);
+        _minSpawnFrequency = _baseMinSpawnFrequency;
+        _maxSpawnFrequency = _baseMaxSpawnFrequency;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(_currentFrequency);
+        
         _elapsedTime += Time.deltaTime;
-
-        if (_elapsedTime >= _spawnFrequency)
+        Debug.Log(Time.time);
+        
+        if (_elapsedTime >= _currentFrequency)
         {
             int projType = Random.Range(0, (int)ProjectileType.Count);
 
@@ -48,6 +71,8 @@ public class ProjectileShooter : MonoBehaviour
             }
 
             _elapsedTime = 0f;
+
+            _currentFrequency = Random.Range(_minSpawnFrequency, _maxSpawnFrequency);
         }
     }
 

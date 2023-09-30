033fefc770670cf72efeb7cc96d7ed0395e5f441
Alexis Haldy <alexishaldy@gmail.com>
2023-09-30 18:44:10 +0200

    Added transition and music on game

---
+++ Assets/Scripts/Ui/GameInit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameInit : MonoBehaviour
+{
+     [SerializeField] private TransitionScreen _transitionScreen;
+     [SerializeField] private MusicPlayer _musicPlayer;
+     
+     private void Awake()
+     {
+         _transitionScreen.FadeOut(1f);
+         _musicPlayer.PlayGame();
+     }
+}

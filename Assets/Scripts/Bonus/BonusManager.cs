f13228d191a7795d49c1e744c1b76feb788ed289
olivier <olivierpachoud2@gmail.com>
2023-09-30 17:42:51 +0200

    script bonus manager

---
+++ Assets/Scripts/Bonus/BonusManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusManager : MonoBehaviour
+{
+    [SerializeField] private List<BonusBase> _bonus;
+
+    [SerializeField] private float _bonusSpawnFrequency;
+
+    private float _elapsedTime;
+
+    private bool _isAnObjectInGame = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_isAnObjectInGame)
+        {
+            _elapsedTime += Time.deltaTime;
+        }
+
+        if (_elapsedTime >= _bonusSpawnFrequency)
+        {
+            int rndIdx = Random.Range(0, _bonus.Count);
+
+            Instantiate(_bonus[rndIdx].gameObject);
+
+            _elapsedTime = 0f;
+
+            _isAnObjectInGame = true;
+        }
+    }
+}

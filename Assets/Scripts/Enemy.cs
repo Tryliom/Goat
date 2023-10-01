b3a9b013dbcb82fa486291aa6af20432f82eeafd
Alexis Haldy <alexishaldy@gmail.com>
2023-10-01 12:23:51 +0200

    Add enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private List<Material> _materials;
+    
+    private void Start()
+    {
+        var mesh = GetComponent<MeshRenderer>();
+        
+        for (var i = 0; i < mesh.materials.Length; i++)
+        {
+            mesh.materials[i] = _materials[Random.Range(0, _materials.Count)];
+        }
+    }
+}

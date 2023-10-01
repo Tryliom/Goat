2dcd809f6b5bb0c29e77b29e1fdd8a3a7b340112
Rem Lamb <rem.lamb@outlook.fr>
2023-10-01 14:30:10 +0200

    ShieldShader

---
+++ Assets/Scripts/ShieldPosition.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldPosition : MonoBehaviour
+{
+    private GameObject[] _goatTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _goatTransform = GameObject.FindGameObjectsWithTag("Goat");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = _goatTransform[0].transform.position;
+    }
+}
